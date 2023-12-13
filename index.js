e078407023580b932c8a87e915910c084db81d81
bhat-huzaif <153756487+bhat-huzaif@users.noreply.github.com>
2023-12-13 22:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-<html>
-  <link rel="stylesheet" href="indes.css">
-  <script>prompt("Hello there! Enter Your Name")</script>
-  <body>
-    
-  </body>
-</html>
+const print(a) = document.write(a)
+let x = prompt("Enter Stating No of loop")
+let y = prompt("Enter Ending No of loop")
+for (print("Loop Started; x<y; x++)
+{
+  console.log(x);
+}
